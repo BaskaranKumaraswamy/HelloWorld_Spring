735c3a9817853a3a983b703def22c95ccb243ebc
Karthiga Baskar <karthigabhaskaran@gmail.com>
2024-07-30 15:47:24 -0400

    Added CreditCardController

---
+++ src/main/java/org/example/interviewprep/controller/CheckinController.java
@@ -0,0 +1,28 @@
+package org.example.interviewprep.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+
+@RestController
+public class CheckinController {
+
+    @GetMapping(value = "bankdetails",produces = MediaType.APPLICATION_JSON_VALUE)
+    public String getMyDetails(){
+        HashMap bankDetails=new HashMap();
+        //ArrayList bankDetails = new ArrayList();
+        bankDetails.put("name","Deshvakumarbaskar");
+        bankDetails.put("acc","4");
+        bankDetails.put("bal","5000");
+        if (bankDetails.containsKey("name1")){
+
+            return bankDetails.get("name").toString();
+
+        } else {
+            return "no key present";
+        }
+
+    }
+}
--- src/main/java/org/example/interviewprep/controller/CreditCardController.java
+++ src/main/java/org/example/interviewprep/controller/CreditCardController.java
@@ -1,15 +1,13 @@
 package org.example.interviewprep.controller;
 
-import org.json.JSONObject;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 
 @RestController
-public class ApplicationController {
+public class CreditCardController {
 
     @GetMapping(value = "bankdetails",produces = MediaType.APPLICATION_JSON_VALUE)
     public String getMyDetails(){
